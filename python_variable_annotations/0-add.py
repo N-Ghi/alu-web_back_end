82da3ad39685423759c0014a02da9e1b10c420af
= <=>
2025-05-07 19:14:38 +0200

    corrected

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 
-def add(a : float, b : float):
-    sum : float = a + b
-    return sum
+def add(a : float, b : float) -> float:
+    return a + b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 
-def concat (str1 : str, str2 : str):
-    string : str = str1+str2
-    return string
+def concat (str1 : str, str2 : str) -> str:
+    return str1+str2
