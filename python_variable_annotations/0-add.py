7348c7960a65674e80f3930c2a63adaf18a4eb24
= <=>
2025-05-07 19:22:32 +0200

    okk

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 
-def add(a : float, b : float) -> float:
-    # inside the function
+def add(a : float, b : float) -> float :
+    # inside the function, more words
     return a + b
