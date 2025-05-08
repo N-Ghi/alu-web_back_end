6c7b39394dce6cc4b74d675c335319cd37343d93
= <=>
2025-05-08 12:15:04 +0200

    some corrections

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""This module provides a function to add two float numbers and return their sum."""
+"""A function to add two float numbers and return their sum."""
+
 
 def add(a: float, b: float) -> float:
     """Return the sum of two float numbers a and b."""
