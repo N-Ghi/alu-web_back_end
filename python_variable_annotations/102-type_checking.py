1831e8950d002cf3dd4dddf451e0e584320a14b8
= <=>
2025-05-08 12:57:25 +0200

    102-type

--- python_variable_annotations/102-type_checking.py
+++ python_variable_annotations/102-type_checking.py
@@ -13,8 +13,8 @@ def zoom_array(lst: Tuple[Any], factor: int = 2) -> List:
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = tuple([12, 72, 91])
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(tuple(array), 3)
