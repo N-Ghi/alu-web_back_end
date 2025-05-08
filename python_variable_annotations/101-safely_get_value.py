26218a47455c8df35cf59ca79a043ed1e15a756e
= <=>
2025-05-08 12:49:11 +0200

    101-safely_

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,7 @@ from typing import TypeVar, Optional, Mapping, Any, Union
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Union[Any, T, None]:
+def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Union[Any, T]:
     """Return key from the dictionary, or default if key is not found."""
     if key in dct:
         return dct[key]
