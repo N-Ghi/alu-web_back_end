58789c2d4fd950b9354b65a9db9af2c11e2227b8
= <=>
2025-05-19 18:46:14 +0200

    1-filtered

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 Module for obfuscating log data
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Obfuscate specified fields in a log message"""
     pattern = ''.join([f'({field}=)[^{separator}]*({separator})' for field in fields])
     return re.sub(pattern, f'\\1{redaction}\\2', message)
