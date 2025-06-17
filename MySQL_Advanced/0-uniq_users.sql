348391455cf1084a73d839ac25c618af2e3200a9
= <=>
2025-06-17 08:44:22 +0200

    0-

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -3,7 +3,8 @@
 -- Check if table exists and create only if it doesn't
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
+
